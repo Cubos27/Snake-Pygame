a9e74e391628c329bc9471cd203b48e177ffe7be
Cubos <cubos2769.andrade@gmail.com>
2024-09-15 23:12:23 -0600

    snake v1 finished
    - fruits added
    - added collision with snake

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 
@@ -11,7 +12,7 @@ class Snake():
             [130, 150],
             [140, 150],
         ]
-        self.speedX = 10
+        self.speedX = 7
         self.speedY = 0
         self.direction = "right"
 
@@ -50,12 +51,33 @@ class Snake():
                 self.snake_body[len(self.snake_body)-1][1] <= 5 or
                 self.snake_body[len(self.snake_body)-1][1] >= 495):
             return True
-        # for point in self.snake_body()
-        # if self.snake_body[len(self.snake_body)-1][0] ==
+        for i in range(1, len(self.snake_body)-2):
+            if (self.snake_body[len(self.snake_body)-1][0] == self.snake_body[i][0] and
+                    self.snake_body[len(self.snake_body)-1][1] == self.snake_body[i][1]):
+                return True
 
     def updateSnake(self):
         self.draw_snake()
         self.snake_movement()
+
+
+class Fruit():
+    def __init__(self):
+        super().__init__()
+        self.posx = random.randint(0, 495)
+        self.posy = random.randint(0, 495)
+
+    def draw_fruit(self):
+        pygame.draw.circle(screen, "Red", (self.posx, self.posy), 7)
+
+    def collision_snake_fruit(self):
+        if (snake.snake_body[len(snake.snake_body)-1][0] <= self.posx+10 and
+                snake.snake_body[len(snake.snake_body)-1][0] >= self.posx-10 and
+                snake.snake_body[len(snake.snake_body)-1][1] <= self.posy+10 and
+                snake.snake_body[len(snake.snake_body)-1][1] >= self.posy-10):
+            snake.snake_body.append([snake.snake_body[len(snake.snake_body)-1][0],
+                                    snake.snake_body[len(snake.snake_body)-1][1]])
+            return True
 
 
 # pygame setup
@@ -72,6 +94,9 @@ instruction_rect = instruction_surf.get_rect(center=(250, 420))
 
 game_active = False
 snake = Snake()
+fruits = []
+for i in range(5):
+    fruits.append(Fruit())
 
 
 while True:
@@ -87,7 +112,7 @@ while True:
                     snake.move_up()
                 if event.key == pygame.K_RIGHT and snake.direction != "left":
                     snake.move_right()
-                if event.key == pygame.K_LEFT and snake.direction != "right":
+                if event.key == pygame.K_LEFT and snake.direction != "right":  # me la pelas
                     snake.move_left()
         else:
             if event.type == pygame.KEYDOWN:
@@ -97,7 +122,24 @@ while True:
         screen.fill("black")
         snake.updateSnake()
 
+        for fruit in fruits:
+            fruit.draw_fruit()
+            if fruit.collision_snake_fruit():
+                fruits.pop(fruits.index(fruit))
+                fruits.append(Fruit())
+
         if snake.snake_collided():
+            snake.move_right()
+            snake.snake_body = [
+                [100, 150],
+                [110, 150],
+                [120, 150],
+                [130, 150],
+                [140, 150],
+            ]
+            fruits = []
+            for i in range(5):
+                fruits.append(Fruit())
             game_active = False
     else:
         screen.fill("black")
