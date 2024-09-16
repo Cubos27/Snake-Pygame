68ea7570508599f64f05c660ee9c1cb80f981738
Cubos <cubos2769.andrade@gmail.com>
2024-09-15 18:01:34 -0600

    Snake added
    - Snake class added
    - collisions with walls added

--- snake.py
+++ snake.py
@@ -4,82 +4,99 @@ import pygame
 class Snake():
     def __init__(self):
         super().__init__()
-        self.snakeBody = [
-            [160, 160],
-            [145, 145],
-            [130, 130],
-            [115, 115],
-            [100, 100],
+        self.snake_body = [
+            [100, 150],
+            [110, 160],
+            [120, 170],
+            [130, 180],
+            [140, 190],
         ]
-        self.speedX = 5
-        self.speedY = 5
+        self.speedX = 10
+        self.speedY = 0
+        self.direction = "right"
 
-    def drawSnake(self):
-        for ball in self.snakeBody:
-            pygame.draw.circle(screen, "blue", (ball), 15)
+    def draw_snake(self):
+        for point in self.snake_body:
+            pygame.draw.circle(screen, "green", (point), 7)
 
-    def snakeMovement(self):
-        for i in range(1, len(self.snakeBody), 1):
-            self.snakeBody[i][1] = self.snakeBody[i-1][0]-10
-            self.snakeBody[i][0] = self.snakeBody[i-1][1]-10
-        # if (self.speedY <= 0):
-        #     for i in range(1, len(self.snakeBody), 1):
-        #         self.snakeBody[i][0] = self.snakeBody[i-1][1] + 12
-        # else:
-        #     for i in range(1, len(self.snakeBody), 1):
-        #         self.snakeBody[i][0] = self.snakeBody[i-1][1] - 12
+    def snake_movement(self):
+        self.snake_body.pop(0)
+        self.snake_body.append([self.snake_body[len(self.snake_body)-1][0] + self.speedX,
+                               self.snake_body[len(self.snake_body)-1][1] + self.speedY])
 
-        # if (self.speedX <= 0):
-        #     for i in range(1, len(self.snakeBody), 1):
-        #         self.snakeBody[i][0] = self.snakeBody[i-1][0] + 12
-        # else:
-        #     for i in range(1, len(self.snakeBody), 1):
-        #         self.snakeBody[i][1] = self.snakeBody[i-1][0] - 12
+    def move_down(self):
+        self.speedX = 0
+        self.speedY = 10
+        self.direction = "down"
 
-        self.snakeBody[0][0] += self.speedX
-        self.snakeBody[0][1] += self.speedY
+    def move_up(self):
+        self.speedX = 0
+        self.speedY = -10
+        self.direction = "up"
 
-    def snakeBounces(self):
-        if (self.snakeBody[0][0] <= 0):
-            self.speedX *= -1
-        if (self.snakeBody[0][0] >= 500):
-            self.speedX *= -1
-        if (self.snakeBody[0][1] <= 0):
-            self.speedY *= -1
-        if (self.snakeBody[0][1] >= 400):
-            self.speedY *= -1
-            # for i in range(len(self.snakeBody)-1, 0, -1):
-            #     self.snakeBody.pop(i)
-            #     self.snakeBody.append([self.snakeBody[i-1][0] - 12,
-            #                            self.snakeBody[i-1][1] + 12])
-            #     print(self.snakeBody)
+    def move_right(self):
+        self.speedX = 10
+        self.speedY = 0
+        self.direction = "right"
+
+    def move_left(self):
+        self.speedX = -10
+        self.speedY = 0
+        self.direction = "left"
 
     def updateSnake(self):
-        self.drawSnake()
-        self.snakeMovement()
-        self.snakeBounces()
-        print(self.snakeBody)
+        self.draw_snake()
+        self.snake_movement()
 
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((500, 400))
+screen = pygame.display.set_mode((500, 500))
 clock = pygame.time.Clock()
-running = True
 
+pixel_font = pygame.font.Font('fonts/Pixeltype.ttf', 50)
+title_surf = pixel_font.render('Snake Game', False, "White")
+title_rect = title_surf.get_rect(center=(250, 80))
+
+instruction_surf = pixel_font.render('Press any key to start', False, "White")
+instruction_rect = instruction_surf.get_rect(center=(250, 420))
+
+game_active = False
 snake = Snake()
 
-while running:
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            exit()
+        if game_active:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN and snake.direction != "up":
+                    snake.move_down()
+                if event.key == pygame.K_UP and snake.direction != "down":
+                    snake.move_up()
+                if event.key == pygame.K_RIGHT and snake.direction != "left":
+                    snake.move_right()
+                if event.key == pygame.K_LEFT and snake.direction != "right":
+                    snake.move_left()
+        else:
+            if event.type == pygame.KEYDOWN:
+                game_active = True
 
-    screen.fill("black")
+    if game_active:
+        screen.fill("black")
+        snake.updateSnake()
 
-    snake.updateSnake()
+        if (snake.snake_body[len(snake.snake_body)-1][0] <= 5 or
+            snake.snake_body[len(snake.snake_body)-1][0] >= 495 or
+            snake.snake_body[len(snake.snake_body)-1][1] <= 5 or
+                snake.snake_body[len(snake.snake_body)-1][1] >= 495):
+            game_active = False
+    else:
+        screen.fill("black")
+        screen.blit(instruction_surf, instruction_rect)
+        screen.blit(title_surf, title_rect)
 
     pygame.display.flip()
-
-    clock.tick(60)
-
-pygame.quit()
+    clock.tick(15)
